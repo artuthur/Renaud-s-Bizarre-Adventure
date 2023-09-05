f779c58dcb3a42dfb101ce6eba7d01354245a324
Kahil Benhaddouche <kahil.benhaddouche.etu@univ-lille.fr>
2023-09-05 09:24:41 +0200

    Ajout des spells et modification du bestiary

--- src/main/Spell.java
+++ src/main/Spell.java
@@ -1,5 +1,25 @@
 package main;
-/* à modifier */
-public class Spell extends Bonus {
-    
+
+public enum Spell {
+    SPELLTEST(SpellType.ATTACK, 20);
+
+    private SpellType type;
+    private int amount;
+
+    private Spell(SpellType type, int amount){
+        this.type = type;
+        this.amount = amount;
+    }
+
+    public SpellType getType(){
+        return type;
+    }
+
+    public int getAmount(){
+        return amount;
+    }
+
+    public String toString(){
+        return getClass().getSimpleName() + "[type:" + type + ", amount:" + amount + "]";
+    }
 }
