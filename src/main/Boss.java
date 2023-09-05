49b9eca26348f0c56b1cbd2be79a7f87d0a2f9eb
Raphaël Pladys <raphael.pladys.etu@univ-lille.fr>
2023-09-05 09:13:28 +0200

    Renaud.java class added

---
+++ src/main/Boss.java
@@ -0,0 +1,7 @@
+package main;
+/**
+ * Boss
+ */
+public class Boss {
+    
+}
---
+++ src/main/Monster.java
@@ -0,0 +1,17 @@
+package main;
+/**
+ * Monster
+ */
+public class Monster extends Entity{
+
+    /**
+     * 
+     * @param pv
+     * @param atq
+     * @param def
+     * @param name
+     */
+    public Monster(int pv, int atq, int def, String name){
+        super(pv, atq, def, name);
+    }
+}
