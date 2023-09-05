0725d66db238b80f27288f9c655fb1cc3d9ee0c9
Raphaël Kiecken <raphael.kiecken.etu@univ-lille.fr>
2023-09-05 14:18:22 +0200

    Progression Battle

--- src/main/Game.java
+++ src/main/Game.java
@@ -42,7 +42,8 @@ public class Game {
         return br.readLine();
     }
 
-    public static void fight() {
-
+    public static void clearScreen(){
+        System.out.println("\033[H\033[2J");
+        System.out.flush();
     }
 }
