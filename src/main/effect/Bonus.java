1d0f5b303fc14613f9e67949740fc26dd223cbe3
Kahil Benhaddouche <kahil.benhaddouche.etu@univ-lille.fr>
2023-09-07 09:39:35 +0200

    Patch note 1.2

--- src/main/effect/Bonus.java
+++ src/main/effect/Bonus.java
@@ -27,28 +27,28 @@ public enum Bonus {
 
     /* Sorts à valeur modulable (value est un pourcentage en int) */
     LA_SERENADE_DU_COEUR("La sérénade du coeur", 4, BonusType.SPELL_SCALING, SpellType.HEALTH, UseType.DAMAGE, 5, 50),
-    HARD_METAL("Hard metal", 4, BonusType.SPELL_SCALING, SpellType.DEFENSE, UseType.DAMAGE, 5, 75),
-    ARIA_OF_THE_SOULS("Aria of the souls", 4, BonusType.SPELL_SCALING, SpellType.ATTACK, UseType.DAMAGE, 5, 100),
-    LA_BERCEUSE_DERESHKIGAL_LULLABY("La berceuse d'Ereshkigal", 4, BonusType.SPELL_SCALING, SpellType.HEALTH, UseType.HEAL, 5, 50),
-    MEGIDOLAON("Megidolaon", 2, BonusType.SPELL_SCALING, SpellType.HEALTH, UseType.DAMAGE, 2, 25),
-    VENT_DE_FARORE("Vent de Farore", 2, BonusType.SPELL_SCALING, SpellType.DEFENSE, UseType.DAMAGE, 2,35),
-    FEU_DE_DIN("Feu de Din", 2, BonusType.SPELL_SCALING, SpellType.ATTACK, UseType.DAMAGE, 2, 50),
+    HARD_METAL("Hard metal", 4, BonusType.SPELL_SCALING, SpellType.DEFENSE, UseType.DAMAGE, 5, 200),
+    ARIA_OF_THE_SOULS("Aria of the souls", 4, BonusType.SPELL_SCALING, SpellType.ATTACK, UseType.DAMAGE, 5, 250),
+    LA_BERCEUSE_DERESHKIGAL_LULLABY("La berceuse d'Ereshkigal", 4, BonusType.SPELL_SCALING, SpellType.HEALTH, UseType.HEAL, 5, 75),
+    MEGIDOLAON("Megidolaon", 2, BonusType.SPELL_SCALING, SpellType.HEALTH, UseType.DAMAGE, 2, 30),
+    VENT_DE_FARORE("Vent de Farore", 2, BonusType.SPELL_SCALING, SpellType.DEFENSE, UseType.DAMAGE, 2,100),
+    FEU_DE_DIN("Feu de Din", 2, BonusType.SPELL_SCALING, SpellType.ATTACK, UseType.DAMAGE, 2, 125),
     SAGESSE_DE_NAYRU("Sagesse de Nayrus", 2, BonusType.SPELL_SCALING, SpellType.HEALTH, UseType.HEAL, 2, 25),
-    MEGIDO("Megido", 0, BonusType.SPELL_SCALING, SpellType.HEALTH, UseType.DAMAGE, 3, 10),
-    FREEDOM("Freedom", 0, BonusType.SPELL_SCALING, SpellType.DEFENSE, UseType.DAMAGE, 3, 15),
-    AGILAON("Agilaon", 0, BonusType.SPELL_SCALING, SpellType.ATTACK, UseType.DAMAGE, 3, 10),
-    DIA("Dia", 0, BonusType.SPELL_SCALING, SpellType.HEALTH, UseType.DAMAGE, 3, 10),
+    MEGIDO("Megido", 0, BonusType.SPELL_SCALING, SpellType.HEALTH, UseType.DAMAGE, 3, 15),
+    FREEDOM("Freedom", 0, BonusType.SPELL_SCALING, SpellType.DEFENSE, UseType.DAMAGE, 3, 50),
+    AGILAON("Agilaon", 0, BonusType.SPELL_SCALING, SpellType.ATTACK, UseType.DAMAGE, 3, 65),
+    DIA("Dia", 0, BonusType.SPELL_SCALING, SpellType.HEALTH, UseType.HEAL, 3, 10),
 
     /* Buffs constants (value est un pourcentage en int) */
-    VIOLENCE("Violence", 0, BonusType.BUFF, SpellType.ATTACK, 10),
-    VIOLENCE_FAROUCHE("Violence", 0, BonusType.BUFF, SpellType.ATTACK, 30),
-    VIOLENCE_DE_SES_MORTS("Violence de ses morts", 0, BonusType.BUFF, SpellType.ATTACK, 50),
-    SANTE("Santé", 0, BonusType.BUFF, SpellType.HEALTH, 10),
-    SANTE_PAS_CREVE("Santé, pas crevé", 0, BonusType.BUFF, SpellType.HEALTH, 30),
-    SANTE_TOUJOURS_DEBOUT("Santé, toujours debout", 0, BonusType.BUFF, SpellType.HEALTH, 50),
-    ROBUSTESSE("Robustesse", 0, BonusType.BUFF, SpellType.DEFENSE, 10),
-    ROBUSTESSE_IVROGNE("Robustesse ivrogne", 1, BonusType.BUFF, SpellType.DEFENSE, 30),
-    ROBUSTESSE_COCKASS("Robustesse cockass", 2, BonusType.BUFF, SpellType.DEFENSE, 50);
+    VIOLENCE("Violence", 0, BonusType.BUFF, SpellType.ATTACK, 20),
+    VIOLENCE_FAROUCHE("Violence", 1, BonusType.BUFF, SpellType.ATTACK, 50),
+    VIOLENCE_DE_SES_MORTS("Violence de ses morts", 2, BonusType.BUFF, SpellType.ATTACK, 100),
+    SANTE("Santé", 0, BonusType.BUFF, SpellType.HEALTH, 20),
+    SANTE_PAS_CREVE("Santé, pas crevé", 1, BonusType.BUFF, SpellType.HEALTH, 50),
+    SANTE_TOUJOURS_DEBOUT("Santé, toujours debout", 2, BonusType.BUFF, SpellType.HEALTH, 100),
+    ROBUSTESSE("Robustesse", 0, BonusType.BUFF, SpellType.DEFENSE, 20),
+    ROBUSTESSE_IVROGNE("Robustesse ivrogne", 1, BonusType.BUFF, SpellType.DEFENSE, 50),
+    ROBUSTESSE_COCKASS("Robustesse cockass", 2, BonusType.BUFF, SpellType.DEFENSE, 100);
 
     private String name;
     private int rarity;
